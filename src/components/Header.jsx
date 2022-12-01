854fec118547b5ba8bcb7626acaf856b77a8d417
AleMMb <alejandramellab@gmail.com>
2022-11-30 22:43:24 -0300

    se aplican estilos a la app con tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
-
-const Header =()=>{
-    return(
-        <h1 className="bg-teal-800 text-amber-900">Feriados en Chile</h1>
-    )
-} 
+const Header = () => {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url(https://images.pexels.com/photos/768474/pexels-photo-768474.jpeg)`
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Feriados Chile 2022</h1>
+          <p className="mb-5">Encuentra los feriados de este año :D</p>
+          <button className="btn btn-primary"><a href="#miApi">vamos</a></button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Header
--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -1,37 +1,63 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
-const MiApi = () =>{
+const MiApi = () => {
+  const [informacion, setInformacion] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
 
-    const [informacion , setInformacion] = useState([])
-    const [busqueda, setBusqueda] = useState('')
+  useEffect(() => {
+    llamadoApi();
+  }, []);
 
-    useEffect(() =>{
-        llamadoApi()
-    },[])
+  const llamadoApi = async () => {
+    const url = "https://api.victorsanmartin.com/feriados/en.json"
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json();
+    setInformacion(datos.data);
+  };
 
-    const llamadoApi = async ()=>{
-        const url="https://api.victorsanmartin.com/feriados/en.json"
-        const respuesta = await fetch (url)
-        const datos = await respuesta.json()
-        setInformacion (datos.data)
-    }
- 
-    const listaFeriado = !busqueda ? informacion : informacion.filter((e) => e.extra.toLowerCase().includes(busqueda.toLocaleLowerCase()))
+  const listaFeriado = !busqueda
+    ? informacion
+    : informacion.filter((e) =>
+        e.extra.toLowerCase().includes(busqueda.toLocaleLowerCase())
+      );
 
-    const ordenando = () => {
-        const ordenarefiado = [...listaFeriado].sort ((a,b)=> a.title > b.title ? 1 : -1)
-        setInformacion (ordenarefiado)
-    }
+  const ordenando = () => {
+    const ordenarefiado = [...listaFeriado].sort((a, b) =>
+      a.title > b.title ? 1 : -1
+    );
+    setInformacion(ordenarefiado);
+  };
 
-    return(
-        <div>
-            <section>
-                <label htmlFor="busqueda">Filtre feriados civiles o religiosos:</label>
-                <input type="text" name='busqueda' placeholder='escriba'onChange={(e) => { setBusqueda(e.target.value)}} value={busqueda} />
-            </section>
-            <h2>{listaFeriado.map(feriado => <li key={feriado.title}>{feriado.date} {feriado.title}</li>)}</h2>
-            <button onClick={ordenando}>Ordenar</button>
-        </div>
-    )
-}
-export default MiApi
+  return (
+    <div className="max-h-max" id="miApi">
+      <section className="h-20 flex justify-evenly content-center p-20">
+        <label htmlFor="busqueda" className="buscador-label h-8 m-0 ">
+          Filtre feriados civiles o religiosos:
+        </label>
+        <input
+          className="h-8 rounded"
+          type="text"
+          name="busqueda"
+          placeholder="buscar..."
+          onChange={(e) => {
+            setBusqueda(e.target.value);
+          }}
+          value={busqueda}
+        />
+      </section>
+      <article className="mx-56">
+        <h2 className="list-none">
+          {listaFeriado.map((feriado) => (
+            <li key={feriado.title}>
+              {feriado.date} {feriado.title}
+            </li>
+          ))}
+        </h2>
+        <button className="btn btn-primary m-10" onClick={ordenando}>
+          mostar por orden alfabético
+        </button>
+      </article>
+    </div>
+  );    
+};
+export default MiApi;
